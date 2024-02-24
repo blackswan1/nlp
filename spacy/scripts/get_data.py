aa4df23b330fe35ee59688f06a42e525a68ce431
Your Name <you@example.com>
2024-02-24 20:35:02 +0100

    initial

--- spacy/scripts/get_data.py
+++ spacy/scripts/get_data.py
@@ -1,8 +1,9 @@
+# Dataframe
 for index, row in ted.iloc[range(0, 20), [ted.columns.get_loc('transcript'), ted.columns.get_loc('url')]].iterrows():
     print('"{transcript}", "{url}"'.format(transcript=row["transcript"], url=row["url"].replace("\n", "")))
 
 
-
+# Series
 for index, value in enumerate(corpus):
     print(f"Index: {index}, Value: {value}")
 
